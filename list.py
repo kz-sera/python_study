34be50956adfc279cb0593495b185306b6a8c41a
kazui sera <kappa-9ri_2389@ezweb.ne.jp>
2018-09-22 20:08:50 +0900

    list.py おわた

--- list.py
+++ list.py
@@ -14,3 +14,22 @@ rand_int = rand.randint(0, 8)
 trump_rand =rand.randint(0, 12)
 trump_card = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 print(trump_card[trump_rand])
+
+print('####################')
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(num_list)
+
+print('####################')
+
+mika_list = ['こ', 'み', 'や', 'ま', 'み', 'か']
+print(mika_list)
+
+print('####################')
+
+print(mika_list[3])
+
+print('####################')
+
+rand_int = rand.randint(0, 5)
+print(mika_list[rand_int])
