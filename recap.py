8e39ae48f1f97cbb9b3a2f7fd8a08cf737827f43
kazui sera <kappa-9ri_2389@ezweb.ne.jp>
2018-09-22 19:52:56 +0900

    recap.py 頑張った

--- recap.py
+++ recap.py
@@ -6,3 +6,31 @@
 #5. 所持金変数を作り好きな金額を代入してください
 #6. 賭け金を聞いて入力を受け付ける変数を作ってください
 #7. ユーザーが入力した数値を所持金から引いた数を"残金: 数字"と出力してください
+
+#1
+first_name = 'kazui'
+print(first_name)
+
+#2
+last_name = 'sera'
+print(last_name)
+
+#3
+age = 43
+print(age)
+
+#4
+over_4 = False
+print(over_4)
+
+#5
+money = 4300
+print('所持金：', money)
+
+#6
+bit = int(input('掛け金を入力してください'))
+print('掛け金:', bit)
+
+#7
+money -= bit
+print('所持金：', money)
