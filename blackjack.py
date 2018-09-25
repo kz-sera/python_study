a5f144299d81d324d0cd7cd5b6c4b41aae714e31
kazui sera <kappa-9ri_2389@ezweb.ne.jp>
2018-09-25 14:47:29 +0900

    python blackjack.py でけた

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 
+import random as rand
 #所持金
 
 #キーボード入力の受付
@@ -9,3 +10,17 @@
 
 #プレイヤーの手を格納する空のリスト
 
+money = 3000
+print('所持金', money)
+
+bit = int(input('掛け金を入力してください'))
+print('掛け金', bit)
+
+trump_card = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+print(trump_card)
+
+trump_rand =rand.randint(0, len(trump_card)-1)
+print(trump_card[trump_rand])
+
+player_hand = []
+print(player_hand)
